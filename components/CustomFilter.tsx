f087359d63042568e93029ed1cc13db9a0997a18
rohansinha01 <rohansinha01@gmail.com>
2024-10-09 18:06:56 -0400

    show more

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -5,15 +5,30 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions, Transition } from '@headlessui/react'
 import { CustomFilterProps } from '@/types'
+import { Router } from 'next/router'
+import { updateSearchParams } from '@/utils'
 
 
 const CustomFilter = ({title, options}: CustomFilterProps) => {
+  const router = useRouter()
   const [selected, setSelected] =useState(options[0])
+
+  const handleUpdateParams = (e: {title: string, value: string}) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase())
+
+
+
+    router.push(newPathName)
+  }
   return (
     <div className='w-fit'>
       <Listbox
         value={selected}
-        onChange={(e) => setSelected(e)}>
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e)
+        
+        }}>
         <div className='relative w-fit z-10'>
           <ListboxButton className="custom-filter__btn">
             <span className='block truncate'>{selected.title}</span>
